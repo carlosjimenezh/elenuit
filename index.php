52de5e8420dfd6ab2f6135fd5ee772d8177c7bfe
Carlos <carlosejimenezh95@gmail.com>
2022-07-13 11:11:54 -0500

    agregar template a proyecto

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insoft</title>
+    <link rel="stylesheet" href="w3pro.css">
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <?php include("header.html"); ?>
+    
+    
+
+    <?php include("footer.html"); ?>
+</body>
+</html>
