1aa999732fd4167421d4a49c4f393de7d3702cf5
Carlos <carlosejimenezh95@gmail.com>
2022-07-15 11:18:42 -0500

    crear html footer

--- footer.php
+++ footer.php
@@ -1,1 +1,39 @@
-footer
+<footer>
+    <div class="contenido">
+    <div class="w3-row">
+        <div class="w3-half mitadizq">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3733.0105051493897!2d-103.42713198545212!3d20.669151605333965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428ae99b26f7a99%3A0xc25a34aae7fa262e!2sAv.%20Ludwig%20Van%20Beethoven%205022%2C%20La%20Estancia%2C%2045030%20Zapopan%2C%20Jal.!5e0!3m2!1sen!2smx!4v1657895489652!5m2!1sen!2smx" 
+                width="100%" 
+                height="200" 
+                style="border:0;" 
+                allowfullscreen="" 
+                loading="lazy" 
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+        </div>
+        <div class="w3-half mitadder">
+            <h3>Tienda Guadalajara</h3>
+            <div>
+                <span class="icono tel"></span>
+                Tel. 33 3629 5481
+            </div>
+            <div>
+                <span class="icono wa"></span>
+                Whatsapp 33 3104 2123
+            </div>
+            <div>
+                <span class="icono dir"></span>
+                Av. Ludwig Van Beethoven 5022, La Estancia, 45030 Zapopan, Jal.
+            </div>
+            <div>
+                <span class="icono mail"></span>
+                guadalajara@elenuit.com
+            </div>
+            <div>
+                <span class="icono clock"></span>
+                Lunes-Viernes 12pm-8pm <br> Sábado 10am-6pm
+            </div>
+        </div>
+    </div>
+    </div>
+</footer>
