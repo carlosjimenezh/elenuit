2ea3b05606872c770871008e46f2333efd0e7a6d
Carlos <carlosejimenezh95@gmail.com>
2022-07-16 13:02:56 -0500

    crear boceto html de sucursal

--- sucursal.php
+++ sucursal.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="w3pro.css">
+    <link rel="stylesheet" href="estilo.css">
     <title>Sucursal</title>
 </head>
 <body>
-    
+    <?php include('header.php'); ?>
+    <main id="rentar">
+        <div class="contenido">
+            <h1>Cómo puedo <strong>rentar</strong> un vestido Elenuit</h1>
+            <img src="assets/prueba.webp" alt="" width="100%">
+        </div>
+    </main>
+    <?php include('footer.php'); ?>
 </body>
 </html>
