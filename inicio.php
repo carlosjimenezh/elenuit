5d34f465031407e18a43127af3c1cfc2e7aca24e
Carlos <carlosejimenezh95@gmail.com>
2022-07-17 13:26:49 -0500

    crear html de inicio

--- inicio.php
+++ inicio.php
@@ -3,9 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="w3pro.css">
+    <link rel="stylesheet" href="estilo.css">
     <title>Inicio</title>
 </head>
 <body>
-    
+    <?php include('header.php'); ?>
+    <main id="inicio">
+        <div class="contenido">
+            <h1>Tienda Guadalajara</h1>
+            <select name="" id=""></select>
+            <select name="" id=""></select>
+            <select name="" id=""></select>
+            <select name="" id=""></select>
+            <select name="" id=""></select>       
+        </div>
+        <div class="items-wrapper">
+            <div class="item"></div>
+            <div class="item"></div>
+            <div class="item"></div>
+            <div class="item"></div>
+        </div>
+    </main>
+    <?php include('footer.php'); ?>
 </body>
 </html>
