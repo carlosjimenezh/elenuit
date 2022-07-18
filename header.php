2535c5c2a997ff246db27badac7c35f58d678d0c
Carlos <carlosejimenezh95@gmail.com>
2022-07-18 00:04:00 -0500

    agregar condición para estilo del link activo

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
     <div class="contenido">
         <img src="assets/logo.png" alt="">
         <div>
-            <a href="inicio.php">Inicio</a>
-            <a href="rentar.php">Como Rentar</a>
-            <a href="sucursal.php">Sucursal</a>
+            <a href="inicio.php" class="<?php if(isset($sec) && $sec==1){ echo "activo";}?>">Inicio</a>
+            <a href="rentar.php" class="<?php if(isset($sec) && $sec==2){ echo "activo";}?>">Como Rentar</a>
+            <a href="sucursal.php" class="<?php if(isset($sec) && $sec==3){ echo "activo";}?>">Sucursal</a>
         </div>
     </div>
 </header>
