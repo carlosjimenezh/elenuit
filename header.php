c1feb5a1008f87109b4445fa6faf75ec40d84c27
Carlos <carlosejimenezh95@gmail.com>
2022-07-17 23:47:01 -0500

    cambiar href de inicio en header

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     <div class="contenido">
         <img src="assets/logo.png" alt="">
         <div>
-            <a href="index.php">Inicio</a>
+            <a href="inicio.php">Inicio</a>
             <a href="rentar.php">Como Rentar</a>
             <a href="sucursal.php">Sucursal</a>
         </div>
