83d2a4190583dfde5a2d4d3eb489db2dec35588f
Carlos <carlosejimenezh95@gmail.com>
2022-07-17 23:26:18 -0500

    crear template de página rentar.php

--- rentar.php
+++ rentar.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="w3pro.css">
+    <link rel="stylesheet" href="estilo.css">
     <title>Cómo rentar</title>
 </head>
 <body>
-    
+    <?php include('header.php'); ?>
+    <main id="rentar">
+        <div class="contenido">
+            <h2 class="w3-center">¿Cómo puedo <strong>rentar</strong> un vestido Elenuit?</h2>
+            <img src="assets/rentar.png" alt="">
+        </div>
+    </main>
+    <?php include('footer.php'); ?>
 </body>
 </html>
